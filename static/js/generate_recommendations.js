8af5469a560d3126b0d08307889ccaa92fadc024
nickdibari <ndibari@fordham.edu>
2021-04-10 22:48:26 -0400

    Refresh playlists for attribute after all songs have been liked

--- static/js/generate_recommendations.js
+++ static/js/generate_recommendations.js
@@ -17,22 +17,44 @@
         }).then(json => {
             let likeButton = document.getElementById('like-button-' + songId);
             likeButton.disabled = true;
+
+            // If every song in the playlist has been liked, generate a new playlist
+            // for the target attribute
+            let target = this.dataset.target;
+            let playlistContainer = document.getElementById(target + '-playlist-container');
+            let likeButtons = playlistContainer.querySelectorAll('button');
+            let allSongsLiked = true;
+
+            for (const button of likeButtons) {
+                if (!button.disabled) {
+                    allSongsLiked = false;
+                }
+            }
+
+            if (allSongsLiked) {
+                while (playlistContainer.hasChildNodes()) {
+                    playlistContainer.removeChild(playlistContainer.firstChild);
+                }
+
+                makeRecommendationRequest(target);
+            }
         })
     }
 
-    function createLikeButton(songId) {
+    function createLikeButton(songId, target) {
         let button = document.createElement('button');
         button.id = 'like-button-' + songId;
         button.className = 'btn btn-success';
         button.appendChild(document.createTextNode('Like'));
         button.dataset.songId = songId;
+        button.dataset.target = target;
         button.addEventListener('click', makeLikeRequest);
 
         return button
     }
 
     function createRecommendationPlaylist(data, target) {
-        let playlistId = target + '-playlist';
+        let playlistId = target + '-playlist-container';
         let playlistErrorMessage = document.getElementById(target + '-playlist-error-message');
         let playlist = document.getElementById(playlistId);
 
@@ -52,7 +74,7 @@
             playButton.setAttribute('allow', 'encrypted-media https://open.spotify.com;');
             playButton.src = 'https://open.spotify.com/embed/track/' + code;
 
-            let likeButton = createLikeButton(code);
+            let likeButton = createLikeButton(code, target);
 
             songContainer.appendChild(playButton);
             songContainer.appendChild(likeButton);
